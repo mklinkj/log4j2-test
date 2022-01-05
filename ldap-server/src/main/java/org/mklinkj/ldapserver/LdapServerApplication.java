0dbad370e0de77da4959e5fc620c141beb7d1869
mklinkj <mklinkj>
2022-01-05 22:45:44 +0900

    테스트 커밋

--- ldap-server/src/main/java/org/mklinkj/ldapserver/LdapServerApplication.java
+++ ldap-server/src/main/java/org/mklinkj/ldapserver/LdapServerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LdapServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LdapServerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(LdapServerApplication.class, args);
+  }
 }
