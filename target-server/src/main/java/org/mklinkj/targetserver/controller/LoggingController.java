9e7a4978320c344807bfcc80adba35bc491ea43d
mklinkj <mklinkl>
2022-01-05 22:41:29 +0900

    테스트 커밋

--- target-server/src/main/java/org/mklinkj/targetserver/controller/LoggingController.java
+++ target-server/src/main/java/org/mklinkj/targetserver/controller/LoggingController.java
@@ -1,12 +1,13 @@
 package org.mklinkj.targetserver.controller;
 
-import java.time.LocalDateTime;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.time.LocalDateTime;
 
 @Log4j2
 @Controller
